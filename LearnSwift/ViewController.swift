b2c2ed1ae142c9828558e6e1362a59d7296a05e2
Lakeba_Prabhu <p.durai@lakeba.com>
2017-10-05 12:55:06 +0530

    Learn Git with Xcode 8

--- LearnSwift/ViewController.swift
+++ LearnSwift/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Learn swift with Git Integration in XCode 8")
+        
     }
 
     override func didReceiveMemoryWarning() {
